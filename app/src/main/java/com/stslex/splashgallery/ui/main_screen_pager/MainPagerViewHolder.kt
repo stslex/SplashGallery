a30ef98b08cba3b73411474186369b42b1cad71c
stslex <ilya977.077@gmail.com>
2021-07-20 14:42:00 +0300

    UI improvements

--- app/src/main/java/com/stslex/splashgallery/ui/main_screen_pager/MainPagerViewHolder.kt
+++ app/src/main/java/com/stslex/splashgallery/ui/main_screen_pager/MainPagerViewHolder.kt
@@ -9,7 +9,7 @@ import com.stslex.splashgallery.utils.downloadAndSetSmallRound
 class MainPagerViewHolder(private val binding: ItemRecyclerPagerMainBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(image: ImageModel) {
-        binding.itemPagerImage.downloadAndSet(image.urls.small)
+        binding.itemPagerImage.downloadAndSet(image.urls.regular)
         binding.itemPagerAuthorName.text = image.user.name
         binding.itemPagerImagePerson.downloadAndSetSmallRound(image.user.profile_image.small)
     }
