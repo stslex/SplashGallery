6fc872a2f0911eb30cb4c1f0973460e85e8dd539
i.stepaniuk <ilya977.077@gmail.com>
2022-09-04 15:50:07 +0300

    all versions are in toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,46 +45,26 @@ android {
 dependencies {
     implementation(project(":core"))
 
-    /*Paging*/
-    val pagingVersion = "3.1.1"
-    implementation("androidx.paging:paging-runtime-ktx:$pagingVersion")
-
-    /*Dagger 2*/
-    val daggerVersion = "2.41"
-    implementation("com.google.dagger:dagger:$daggerVersion")
-    kapt("com.google.dagger:dagger-compiler:$daggerVersion")
-
-    /*Glide*/
-    val glideVersion = "4.13.1"
-    implementation("com.github.bumptech.glide:glide:$glideVersion")
-    annotationProcessor("com.github.bumptech.glide:compiler:$glideVersion")
-
-    /*Lifecycle components*/
-    val lifecycleVersion = "2.4.1"
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-
-    /*Coroutines*/
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
-
-    /*Retrofit*/
-    val retrofitVersion = "2.9.0"
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.6")
-
-    /*Navigation Component*/
-    val navVersion = "2.4.2"
-    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
-
-    /*Default implementations*/
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.7.0-beta01")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    libs.apply {
+        implementation(androidx.paging.runtime)
+        implementation(google.dagger.core)
+        kapt(google.dagger.compiler)
+        implementation(glide.core)
+        annotationProcessor(glide.compiler)
+        implementation(androidx.lifecycle.livedata)
+        implementation(androidx.lifecycle.runtime)
+        implementation(androidx.lifecycle.viewmodel)
+        implementation(retrofit.core)
+        implementation(retrofit.converter.gson)
+        implementation(okhttp3.logging.interceptor)
+        implementation(androidx.navigation.fragment)
+        implementation(androidx.navigation.ui)
+        implementation(androidx.core)
+        implementation(androidx.appcompat)
+        implementation(google.material)
+        implementation(androidx.constraintlayout)
+        testImplementation(junit4)
+        androidTestImplementation(androidx.test.junit)
+        androidTestImplementation(androidx.test.espresso)
+    }
 }
